78ba1ace76bf541e4d6c2b897bd51861510c7eee
Agustina Balcarcel <balcarcelagustina@gmail.com>
2024-07-02 19:21:37 -0300

    Initial commit

---
+++ Backend/Core/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Core
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddCoreServiceCollection(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ Backend/Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services) 
+        {
+            return services;
+        }
+    }
+}
