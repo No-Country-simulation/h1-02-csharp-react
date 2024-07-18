5b4ccf2d53d404c7e2053ea15b032e5d57eeb522
Olivera Leonardo <olezdev@gmail.com>
2024-07-18 15:38:55 -0300

    feat: add authentication profile mapper

---
+++ Backend/Mappings/Profiles/Authentication/AuthenticationProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using DTOs.Authentication;
+
+namespace Mappings.Profiles.Authentication;
+
+public class AuthenticationProfile : Profile
+{
+    public AuthenticationProfile()
+    {
+        CreateMap<ApplicationUser, AuthenticatedUserReponse>();
+
+        //CreateMap<ApplicationUser, AuthenticatedUserReponse>().ReverseMap()
+        //    .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+        //    .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName));
+    }
+}
