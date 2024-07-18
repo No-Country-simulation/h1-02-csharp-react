e5529b50d2e38debab8fb56e576aaa1e9af3ff8f
Olivera Leonardo <olezdev@gmail.com>
2024-07-18 15:36:48 -0300

    chore: edit authentication response

--- Backend/DTOs/Authentication/AuthenticationResponse.cs
+++ Backend/DTOs/Authentication/AuthenticationResponse.cs
@@ -3,8 +3,5 @@
 
 public class AuthenticationResponse
 {
-    public Guid Id { get; set; }
-    public string UserName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
     public string Token { get; set; } = string.Empty;
 }
