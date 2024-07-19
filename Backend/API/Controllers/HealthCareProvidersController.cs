9ea255ad76f53465bd6f61e67ca0e27aeb77317a
Olivera Leonardo <olezdev@gmail.com>
2024-07-19 16:22:35 -0300

    feat: add HealthCareProvidesController to update

---
+++ Backend/API/Controllers/HealthCareProvidersController.cs
@@ -0,0 +1,37 @@
+﻿using Application.Contracts.Services;
+using DTOs.HealthCareProvider;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HealthCareProvidersController : ControllerBase
+{
+    private readonly IHealthCareProviderService _healthCareProviderService;
+
+    public HealthCareProvidersController(IHealthCareProviderService healthCareProviderService)
+    {
+        _healthCareProviderService = healthCareProviderService;
+    }
+
+    [HttpPut]
+    [Authorize(Roles = "HealthCareProvider")]
+    public async Task<IActionResult> UpdateHealthCareProvider(HealthCareProviderUpdateDto updateDto)
+    {
+        var userId = User.FindFirstValue("uid");
+
+        if (userId == null)
+        {
+            return Unauthorized();
+        }
+        //var id = new Guid(userId);
+        var result = await _healthCareProviderService.UpdateHealthCareProviderAsync(userId, updateDto);
+        if (!result) return NotFound();
+
+        return NoContent();
+    }
+}
