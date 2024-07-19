83c2b795b8d44e3e2febd607db26dafea5172b6f
Olivera Leonardo <olezdev@gmail.com>
2024-07-19 16:21:08 -0300

    feat: add predicate method to getby and getall

--- Backend/Infrastructure/Repositories/GenericRepository.cs
+++ Backend/Infrastructure/Repositories/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Persistence.Data;
 using Application.Contracts.Persistence;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Persistence.Repositories;
 
@@ -23,6 +24,11 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
         return await _dbContext.Set<T>().ToListAsync();
     }
 
+    public async Task<IReadOnlyList<T>> GetAllPredicateAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbContext.Set<T>().Where(predicate).ToListAsync();
+    }
+
     public async Task<T> AddAsync(T entity)
     {
         await _dbContext.Set<T>().AddAsync(entity);
@@ -31,14 +37,19 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
 
     public async Task UpdateAsync(T entity)
     {
-        // implement
+        _dbContext.Set<T>().Update(entity);
+
     }
 
     // implement soft delete
     public async Task DeleteAsync(T entity)
     {
-        //_dbContext.Set<T>().Remove(entity);
-        //await _dbContext.SaveChangesAsync();
+        _dbContext.Set<T>().Remove(entity);
+    }
+
+    public async Task<T> GetByConditionAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbContext.Set<T>().FirstOrDefaultAsync(predicate);
     }
 
     public async Task SaveChangesAsync()
