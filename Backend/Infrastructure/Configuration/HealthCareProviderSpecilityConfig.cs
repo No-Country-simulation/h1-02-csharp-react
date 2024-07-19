883dd8b3a58a961b56a5c4a4f340b75bb4bb0a50
Olivera Leonardo <olezdev@gmail.com>
2024-07-19 16:15:53 -0300

    feat: edit HealthCareProviderSpeciality

---
+++ Backend/Infrastructure/Configuration/HealthCareProviderSpecilityConfig.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configuration;
+
+public class HealthCareProviderSpecilityConfig : IEntityTypeConfiguration<HealthCareProviderSpeciality>
+{
+    public void Configure(EntityTypeBuilder<HealthCareProviderSpeciality> builder)
+    {
+        builder
+            .HasKey(hps => new { hps.HealthCareProviderId, hps.SpecialityId });
+
+        builder
+            .HasOne(hps => hps.HealthCareProvider)
+            .WithMany(hp => hp.HealthCareProviderSpecialities)
+            .HasForeignKey(hps => hps.HealthCareProviderId);
+
+        builder
+            .HasOne(hps => hps.Speciality)
+            .WithMany(s => s.HealthCareProviderSpecialities)
+            .HasForeignKey(hps => hps.SpecialityId);
+    }
+
+}
