faa43f62ea91c72a883fe7b2b3e0f397211fa875
Olivera Leonardo <olezdev@gmail.com>
2024-07-23 16:02:17 -0300

    feat: add SpecilitiesController

---
+++ Backend/DTOs/Speciality/GetSpecialityDto.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace DTOs.Speciality;
+
+public class GetSpecialityDto
+{
+    //public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public ICollection<string> HealthCareProviders { get; set; } = new List<string>();
+
+}
