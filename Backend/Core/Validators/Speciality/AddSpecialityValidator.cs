e2d01c044e5e973e95f287f8663d749157bac4bf
Olivera Leonardo <olezdev@gmail.com>
2024-07-23 16:01:02 -0300

    feat: add validator to AddSpeciality

---
+++ Backend/Core/Validators/Speciality/AddSpecialityValidator.cs
@@ -0,0 +1,14 @@
+﻿using DTOs.Speciality;
+using FluentValidation;
+
+namespace Application.Validators.Speciality;
+
+public class AddSpecialityValidator : AbstractValidator<AddSpecialityDto>
+{
+    public AddSpecialityValidator()
+    {
+        RuleFor(p => p.Description)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .MaximumLength(60).WithMessage("{PropertyName} cannot be more than {MaxLength} characters");
+    }
+}
--- Backend/DTOs/ServiceResponse.cs
+++ Backend/DTOs/ServiceResponse.cs
@@ -1,9 +1,39 @@
-﻿namespace DTOs
+﻿namespace DTOs;
+
+public class ServiceResponse<T>
 {
-    public class ServiceResponse<T>
+    public T? Data { get; set; }
+    public bool Success { get; set; } = true;
+    public string Message { get; set; } = string.Empty;
+    public List<string>? ValidationErrors { get; set; }
+
+    public ServiceResponse()
     {
-        public T? Data { get; set; }
-        public bool Success { get; set; } = true;
-        public string Message { get; set; } = string.Empty;
+        Success = true;
+    }
+
+    public ServiceResponse(T? data)
+    {
+        Data = data;
+    }
+
+    public ServiceResponse(string message)
+    {
+        Success = true;
+        Message = message;
+    }
+
+    public ServiceResponse(string message, bool success)
+    {
+        Success = success;
+        Message = message;
+    }
+
+    public ServiceResponse(T? data, bool success, string message, List<string>? validationErrors)
+    {
+        Data = data;
+        Success = success;
+        Message = message;
+        ValidationErrors = validationErrors;
     }
 }
