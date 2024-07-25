11cdebc963f01855d14513a357660e6b9ebb008b
Olivera Leonardo <olezdev@gmail.com>
2024-07-25 14:01:07 -0300

    feat: add Register MedicalCenter

--- Backend/API/Controllers/AccountController.cs
+++ Backend/API/Controllers/AccountController.cs
@@ -49,6 +49,26 @@ public class AccountController : ControllerBase
         return Ok(await _authenticationService.RegisterAsync(request));
     }
 
+    [HttpPost("register-medical-center")]
+    [Authorize(Roles = "Admin")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<RegistrationResponse>> RegisterMedicalCenterAsync(RegistrationMedicalCenterRequest request)
+    {
+        return Ok(await _authenticationService.RegisterMedicalCenterAsync(request));
+    }
+
+    [HttpPost("register-health-care-provider")]
+    [Authorize(Roles = "MedicalCenter")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<RegistrationResponse>> RegisterHealthCareProviderAsync(RegistrationHealthCareProviderRequest request)
+    {
+        return Ok(await _authenticationService.RegisterHealthCareProviderAsync(request));
+    }
+
     [HttpGet("me")]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public async Task<ActionResult<AuthenticatedUserReponse>> GetCurrentUser()
---
+++ Backend/Infrastructure/Repositories/MedicalCenterRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Contracts.Persistence;
+using Domain.Entities;
+using Persistence.Data;
+
+namespace Persistence.Repositories;
+
+public class MedicalCenterRepository : GenericRepository<MedicalCenter>, IMedicalCenterRepository
+{
+    public MedicalCenterRepository(JustinaDbContext dbContext) : base(dbContext)
+    {
+    }
+
+}
