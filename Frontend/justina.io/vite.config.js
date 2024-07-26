0450e5102973f6e3176f5c18391b0d2f471facee
Leandro Rossi <leanmatador@gmail.com>
2024-07-26 12:30:39 -0300

    Deploy tests3

--- Frontend/justina.io/vite.config.js
+++ Frontend/justina.io/vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
+  plugins: [react()]  
 })
