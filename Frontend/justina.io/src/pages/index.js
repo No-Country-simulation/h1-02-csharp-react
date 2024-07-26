286ccf679b408adf8a5d1198fb578b7fbbad3913
Leandro Rossi <leanmatador@gmail.com>
2024-07-26 13:04:06 -0300

    Deploy tests3

--- Frontend/justina.io/src/pages/index.js
+++ Frontend/justina.io/src/pages/index.js
@@ -3,13 +3,11 @@ const Landing = lazy(() => import('./Landing/LandingContainer'))
 const Login = lazy(() => import('./Login/LoginContainer'))
 const Register = lazy(() => import('./Register/RegisterContainer'))
 const DrDashboard = lazy(() => import('./DrDashboard/DrDashboardContainer'))
-
 const PatientDetails= lazy(() => import('./PatientDetails/PatientDetailsContainer'))
 const PrescriptionForm= lazy(() => import('./PrescriptionForm/PrescriptionFormContainer'))
 const TreatmentForm= lazy(() => import('./TreatmentForm/TreatmentFormContainer'))
 const PatologyForm= lazy(() => import('./PatologyForm/PatologyFormContainer'))
 const MedicalRecord= lazy(() => import('./MedicalRecord/MedicalRecordContainer'))
-
 const Home = lazy(() => import('./Home/HomeContainer'))
 const ListadoPacientes = lazy(() => import('./Pacientes/ListadoPacientesContainer'))
 const DrProfile = lazy(() => import('./DrProfile/DrProfileContainer'))
