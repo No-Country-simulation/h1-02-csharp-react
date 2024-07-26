6dae75aa66f6d31a3c7f29b38ab7a062303f0a76
Olivera Leonardo <olezdev@gmail.com>
2024-07-26 15:42:45 -0300

    feat: add endpoint for all MedicalCenters and getById

--- Backend/Core/Contracts/Persistence/IMedicalCenterRepository.cs
+++ Backend/Core/Contracts/Persistence/IMedicalCenterRepository.cs
@@ -4,5 +4,6 @@ namespace Application.Contracts.Persistence;
 
 public interface IMedicalCenterRepository : IGenericRepository<MedicalCenter>
 {
-
+    Task<List<MedicalCenter?>> GetMedicalCentersWithUserAsync();
+    Task<MedicalCenter?> GetByIdWithHealthCareProviderAsync(Guid id);
 }
---
+++ Backend/DTOs/MedicalCenter/GetMedicalCentersDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DTOs.MedicalCenter;
+
+public class GetMedicalCentersDto
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string CUIT { get; set; } = string.Empty;
+}
