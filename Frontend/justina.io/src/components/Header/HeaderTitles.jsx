04879cd4e67c0ca35fb2e2d9dfedd0e917960077
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-07-31 18:09:14 -0300

    feat: add profile in header title

--- Frontend/justina.io/src/components/Header/HeaderTitles.jsx
+++ Frontend/justina.io/src/components/Header/HeaderTitles.jsx
@@ -1,15 +1,26 @@
 import useUserStore from "../../hooks/useUserStore";
+import doctorProfile from "../../assets/imgs/medico.png";
+import userProfile from "../../assets/imgs/avatar.png";
 
 export default function HeaderTitles() {
   const { user } = useUserStore();
+  const isPatient = user.roles === "Patient";
+  const profile = isPatient ? userProfile : doctorProfile;
 
   return (
     <div className="pr-[36.24px] p-4 bg-neutral-50/opacity-10 rounded-2xl ">
-      <p className="text-neutral-600 text-[40px] font-bold font-['Noto Sans'] leading-[48px]">
+      <p className="text-neutral-600 text-[40px] font-bold font-['Noto Sans'] leading-[48px] flex gap-x-4 justify-start items-center">
+        <img
+          src={profile}
+          className="rounded-full object-cover object-center size-9"
+          alt="User profile"
+          loading="lazy"
+          decoding="async"
+        />{" "}
         Buen día {user.fullname}
       </p>
       <p className="text-neutral-600 text-subtitulo font-normal font-['Noto Sans'] leading-tight">
-        {user.roles === "Patient"
+        {isPatient === "Patient"
           ? "Aqui puedes registrar todos tus tratamientos"
           : "Revisa el progreso de los pacientes y los tratamientos"}
       </p>
