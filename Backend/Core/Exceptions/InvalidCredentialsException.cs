4c71e790021c7e927ce2d94d6e073170504c9940
Olivera Leonardo <olezdev@gmail.com>
2024-07-31 10:03:30 -0300

    feat: add exception to Authenticate

---
+++ Backend/Core/Exceptions/InvalidCredentialsException.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Exceptions;
+
+public class InvalidCredentialsException : Exception
+{
+    public InvalidCredentialsException() : base("The credentials are not valid.") { }
+}
---
+++ Backend/Core/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Exceptions;
+
+public class UserNotFoundException : Exception
+{
+    public UserNotFoundException(string email) : base($"User with email {email} not found.") { }
+}
