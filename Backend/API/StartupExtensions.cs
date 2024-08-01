2b1fbc68b7a7d3550830b7d8f83163d9f81e32af
Olivera Leonardo <olezdev@gmail.com>
2024-08-01 15:51:16 -0300

    feat: Enhanced Swagger documentation to include authorization details.

--- Backend/API/StartupExtensions.cs
+++ Backend/API/StartupExtensions.cs
@@ -9,6 +9,8 @@ using Persistence.Data;
 using Persistence.Identity;
 using RealTime;
 using AWS;
+using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace API;
 
@@ -94,6 +96,9 @@ public static class StartupExtensions
 
             });
 
+            c.OperationFilter<SwaggerAuthorizeCheckOperationFilter>();
+
+
             c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
                 Description = @"JWT Authorization header using the Bearer scheme. \n\n 
@@ -162,6 +167,28 @@ public static class StartupExtensions
         {
             var logger = scope.ServiceProvider.GetRequiredService<ILogger>();
             logger.LogError(ex, "An error occurred while migrating the database.");
+        }
+    }
+
+    public class SwaggerAuthorizeCheckOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var authorizeAttributes = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+                .Union(context.MethodInfo.GetCustomAttributes(true))
+                .OfType<AuthorizeAttribute>();
+
+            if (authorizeAttributes.Any())
+            {
+                var roles = authorizeAttributes
+                    .Where(attr => !string.IsNullOrEmpty(attr.Roles))
+                    .Select(attr => attr.Roles)
+                    .Distinct();
+
+                var rolesText = roles.Any() ? $"Roles: {string.Join(", ", roles)}" : "Authorization required";
+
+                operation.Description += $"<br/><b>{rolesText}</b>";
+            }
         }
     }
 }
