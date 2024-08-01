552104df924a3db16f9c9b4637638b5ed05935d9
Leandro Rossi <leanmatador@gmail.com>
2024-08-01 17:38:41 -0300

    Update: PatientDetails

--- Frontend/justina.io/src/router.jsx
+++ Frontend/justina.io/src/router.jsx
@@ -65,7 +65,7 @@ export const router = createBrowserRouter([
         element: createPrivateRoute(Home),
       },
       {
-        path: "/patientdetails",
+        path: "/patientdetails/:id",
         element: createPrivateRoute(PatientDetails),
       },
       {
