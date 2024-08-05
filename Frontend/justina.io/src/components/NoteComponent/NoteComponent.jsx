e20fbbf97f5e36bd65568df494b20eba1aab4e6b
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-08-05 01:32:45 -0300

    memoized note component

--- Frontend/justina.io/src/components/NoteComponent/NoteComponent.jsx
+++ Frontend/justina.io/src/components/NoteComponent/NoteComponent.jsx
@@ -1,6 +1,8 @@
 import NoteTextarea from "./NoteTextarea";
 import useHandleDeleteNote from "../../hooks/useHandleDeleteNote";
 import { CrossIcon } from "../icons";
+import { memo } from "react";
+import { useMemo } from "react";
 
 function getRandomClass() {
   const classes = [
@@ -13,35 +15,40 @@ function getRandomClass() {
   return [classes[randomIndex], classes[second]];
 }
 
-export default function NoteComponent({ title, desc, id, i = 0 }) {
-  const { handleDeleteNote } = useHandleDeleteNote();
+const MemoizedNoteComponent = memo(
+  function NoteComponent({ title, desc, id, i = 0 }) {
+    const { handleDeleteNote } = useHandleDeleteNote();
 
-  const handleDelete = () => {
-    handleDeleteNote(id);
-  };
-  const classess = getRandomClass();
-  return (
-    <div
-      className={`shadow-glass-effect rounded-2xl max-w-[400px] h-[175px] p-1 pb-6 ${
-        i % 2 === 0 ? classess[0] : classess[1]
-      }`}
-    >
-      <div className="flex justify-between pl-4 pr-2">
-        <input
-          className="text-primary font-bold text-xl bg-transparent outline-none border-none max-w-[75%]"
-          defaultValue={title || "No title"}
-          disabled
-        />
-        <div>
-          <button
-            className="text-error-200 rounded-full p-1 bg-white/60 w-8 font-bold scale-[0.9]"
-            onClick={handleDelete}
-          >
-            <CrossIcon />
-          </button>
+    const handleDelete = () => {
+      handleDeleteNote(id);
+    };
+    const classess = useMemo(() => getRandomClass(), []);
+    return (
+      <div
+        className={`shadow-glass-effect rounded-2xl max-w-[400px] h-[175px] p-1 pb-6 ${
+          i % 2 === 0 ? classess[0] : classess[1]
+        }`}
+      >
+        <div className="flex justify-between pl-4 pr-2">
+          <input
+            className="text-primary font-bold text-xl bg-transparent outline-none border-none max-w-[75%]"
+            defaultValue={title || "No title"}
+            disabled
+          />
+          <div>
+            <button
+              className="text-error-200 rounded-full p-1 bg-white/60 w-8 font-bold scale-[0.9]"
+              onClick={handleDelete}
+            >
+              <CrossIcon />
+            </button>
+          </div>
         </div>
+        <NoteTextarea desc={desc} />
       </div>
-      <NoteTextarea desc={desc} />
-    </div>
-  );
-}
+    );
+  },
+  (prev, next) => prev.id
+);
+
+export default MemoizedNoteComponent;
